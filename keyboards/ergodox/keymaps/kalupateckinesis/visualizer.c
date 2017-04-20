0261e4b55f5ccb013f4b45f7d17759ebedd4a18e
Paul Kalupnieks <paul@unbounce.com>
2017-04-20 14:45:01 -0700

    Tweaks LCD output

--- keyboards/ergodox/keymaps/kalupateckinesis/visualizer.c
+++ keyboards/ergodox/keymaps/kalupateckinesis/visualizer.c
@@ -23,13 +23,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Also make sure that the buffer passed to state->layer_text remains valid until the previous animation is
 // stopped. This can be done by either double buffering it or by using constant strings
 static void get_visualizer_layer_and_color(visualizer_state_t* state) {
-    uint8_t saturation = 60;
-    if (state->status.leds & (1u << USB_LED_CAPS_LOCK)) {
-        saturation = 255;
+    uint8_t saturation = 128;
+    /* if (state->status.leds & (1u << USB_LED_CAPS_LOCK)) { */
+    /*     saturation = 255; */
+    /* } */
+    if (state->status.layer & 0x6) {
+      state->target_lcd_color = LCD_COLOR(0, saturation, 0xFF);
+      state->layer_text = "Special";
     }
     if (state->status.layer & 0x4) {
         state->target_lcd_color = LCD_COLOR(0, saturation, 0xFF);
-        state->layer_text = "Media & Mouse";
+        state->layer_text = "Media";
     }
     else if (state->status.layer & 0x2) {
         state->target_lcd_color = LCD_COLOR(168, saturation, 0xFF);
@@ -37,6 +41,6 @@ static void get_visualizer_layer_and_color(visualizer_state_t* state) {
     }
     else {
         state->target_lcd_color = LCD_COLOR(84, saturation, 0xFF);
-        state->layer_text = "Default";
+        state->layer_text = "";
     }
 }
